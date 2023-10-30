02b1a7e00071fdbd08eacd267dc55b1b1fc248bf
Geobellz22 <st377126@gmail.com>
2023-10-30 20:31:26 +0000

    Task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python3
-"""Determines if a given data set represents
-a valid UTF-8 encoding"""
+"""
+Determines if a given data set represents a valid UTF-8 encoding
+"""
+
+import unicodedata as code
 
 
 def validUTF8(data):
     """check for valid utf-8 encoding"""
-    bytes_to_check = 0
+    num_bytes = 0
 
-    for byte in data:
-        if bytes_to_check == 0:
-            if (byte >> 5) == 0b110:
-                bytes_to_check = 1
-            elif (byte >> 4) == 0b1110:
-                bytes_to_check = 2
-            elif (byte >> 3) == 0b11110:
-                bytes_to_check = 3
-            elif (byte >> 7) == 0b0:
+    for num in data:
+        if num_bytes == 0:
+            if (num >> 7) == 0b0:
                 continue
+            elif (num >> 5) == 0b110:
+                num_bytes = 1
+            elif (num >> 4) == 0b1110:
+                num_bytes = 2
+            elif (num >> 3) == 0b11110:
+                num_bytes = 3
             else:
                 return False
-        else:
-            if (byte >> 6) != 0b10:
-                return False
-            bytes_to_check -= 1
 
-    return bytes_to_check == 0
+        else:
+            if (num >> 6) != 0b10:
+                return False
+
+            num_bytes -= 1
+
+            if num_bytes == 0:
+                continue
+
+    return num_bytes == 0
